8a7d7e4a899d5d377504f9b480c5d5e8deb628b8
CDM <CDM>
2022-11-14 09:09:21 -0700

    Initial Commit

---
+++ ExamenLomeliCrud/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ExamenLomeliCrud
+//
+//  Created by CCDM29 on 14/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ExamenLomeliCrud/ExamenLomeliCrudApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExamenLomeliCrudApp.swift
+//  ExamenLomeliCrud
+//
+//  Created by CCDM29 on 14/11/22.
+//
+
+import SwiftUI
+
+@main
+struct ExamenLomeliCrudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
