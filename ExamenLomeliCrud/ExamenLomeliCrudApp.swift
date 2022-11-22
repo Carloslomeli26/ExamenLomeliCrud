cd85083a7debae5a39b12e6b7403821f2b8ae8ac
Carloslomeli26 <91562405+Carloslomeli26@users.noreply.github.com>
2022-11-22 10:02:40 -0700

    Update ExamenLomeliCrudApp.swift
    modficacion

--- ExamenLomeliCrud/ExamenLomeliCrudApp.swift
+++ ExamenLomeliCrud/ExamenLomeliCrudApp.swift
@@ -1,20 +1,11 @@
-//
-//  ExamenLomeliCrudApp.swift
-//  ExamenLomeliCrud
-//
-//  Created by CCDM29 on 14/11/22.
-//
-
 import SwiftUI
+
 @main
-
-struct ExamenLomeliCrudApp: App {
-    let persistenceController = PersistenceController.shared
-
+struct ExamenLomeliCruddApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        
         }
     }
 }
