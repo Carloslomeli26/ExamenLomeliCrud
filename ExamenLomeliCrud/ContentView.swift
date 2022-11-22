b83d1d00f552fc2b58a4f6b4c2e8b14930868dce
Carloslomeli26 <91562405+Carloslomeli26@users.noreply.github.com>
2022-11-22 10:02:11 -0700

    Update ContentView.swift
    avance del agregar, editar y borrar, mostrar y menu

--- ExamenLomeliCrud/ContentView.swift
+++ ExamenLomeliCrud/ContentView.swift
@@ -1,44 +1,80 @@
-//
-//  ContentView.swift
-//  ExamenLomeliCrud
-//
-//  Created by CCDM29 on 14/11/22.
-//
-
 import SwiftUI
-import CoreData
 
 struct ContentView: View {
-    @Enviroment (\.managedObjectContext) var context
-    @FetchRequest(entity: Empleados.entity(), sortDescriptors:  []) var empleados: FetchedResults<Empleados>
-    @State private var nuevoempleadonom = ""
-
+    let coreDM: Persistence
+    @State var id = ""
+    @State var empNombre = ""
+    @State var empApePat = ""
+    @State var empApeMat = ""
+    @State var domicilio = ""
+    @State var telefono = ""
     
-    var body: some View {
-        VStack      {
-            TextField("ADD NEW", text: self.$nuevoempleadonom).multilineTextAlignment(.center)
-            Button("Save"){self.guardar()}
-            List{
-                ForEach(empleados, id:\.self){ empleado in
-                    Text("\(empleado.name!)")
+    @State var nid = ""
+    @State var nempNombre = ""
+    @State var nempApePat = ""
+    @State var nempApeMat = ""
+    @State var ndomicilio = ""
+    @State var ntelefono = ""
+    @State var seleccionado: Empleados?
+    @State var empArray = [Empleados]()
 
-                }
-                .onDelete{ indexSet in
-                    for index in indexSet{
-                        self.context.delete(self.empleados[index])
-                        try? self.context.guardar()
+
+    var body: some View {
+        NavigationView{
+            VStack{
+                NavigationLink(destination: VStack{
+                    TextField("ID Empleado:", text: self.$nid).multilineTextAlignment(.center)
+                    TextField("Nombre Empleado:", text: self.$nempNombre).multilineTextAlignment(.center)
+                    TextField("Apellido Paterno", text: self.$nempApePat).multilineTextAlignment(.center)
+                    TextField("Apellido Materno", text: self.$nempApeMat).multilineTextAlignment(.center)
+                    TextField("Domicilio:", text: self.$ndomicilio).multilineTextAlignment(.center)
+                    TextField("Telefono:", text: self.$ntelefono).multilineTextAlignment(.center)
+
+                    Button("Guardar"){
+                        coreDM.guardarEmpleado(id: nid, empNombre: nempNombre, empApePat: nempApePat, empApeMat: nempApeMat, domicilio: ndomicilio, telefono: ntelefono)
+                        nid = ""
+                        nempNombre = ""
+                        nempApePat = ""
+                        nempApeMat = ""
+                        ndomicilio = ""
+                        ntelefono = ""
+                        mostrarEmpleado()
                     }
-                    //hola
+                    }){
+                    Text("Agregar")
                 }
-                
+
+                List{
+                    ForEach(empArray, id: \.self){
+                        emp in
+                        VStack{
+                            Text(emp.empNombre ?? "")
+                        }
+                        .onTapGesture{
+                            seleccionado = emp
+                            id = emp.id ?? ""
+                        }
+                    }.onDelete(perform: {
+                        indexSet in
+                        indexSet.forEach({ index in
+                        let empleado = empArray[index]
+                            coreDM.borrarEmpleado(empleado: <#T##Empleados#>)
+                        mostrarEmpleado()
+                        })
+                    })
+                }.padding()
+                    .onAppear(perform: {mostrarEmpleado()})
             }
         }
-            
     }
-    func guardar(){
-        let nuevoempleado = Empleados(context: self.context)
-        nuevoempleado.name = nuevoempleadonom
-        try? self.context.guardar()
+    func mostrarEmpleado(){
+            empArray = coreDM.leerTodosLosEmpleados()
+        }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView(coreDM: CoreDataManager())
     }
 }
 
