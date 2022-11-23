c0f014caf223729cc27b4362d1d6f510d92d2952
Carloslomeli26 <91562405+Carloslomeli26@users.noreply.github.com>
2022-11-23 09:48:28 -0700

    se instancio la clase coredatamanager

--- ExamenLomeliCrud/ExamenLomeliCrudApp.swift
+++ ExamenLomeliCrud/ExamenLomeliCrudApp.swift
@@ -4,7 +4,7 @@ import SwiftUI
 struct ExamenLomeliCruddApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(coreDM: CoreDataManager())
         
         }
     }
