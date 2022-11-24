234033e48e99ff6983e8754c5e48ba6288d4350d
Carloslomeli26 <91562405+Carloslomeli26@users.noreply.github.com>
2022-11-24 09:34:20 -0700

    Update ContentView.swift
    ya agrega, elimana y edita

--- ExamenLomeliCrud/ContentView.swift
+++ ExamenLomeliCrud/ContentView.swift
@@ -1,3 +1,10 @@
+//
+//  ContentView.swift
+//  ExamenLomeliCrudd
+//
+//  Created by CCDM29 on 22/11/22.
+//
+
 import SwiftUI
 
 struct ContentView: View {
@@ -8,52 +15,63 @@ struct ContentView: View {
     @State var empApeMat = ""
     @State var domicilio = ""
     @State var telefono = ""
-    
-    @State var nid = ""
-    @State var nempNombre = ""
-    @State var nempApePat = ""
-    @State var nempApeMat = ""
-    @State var ndomicilio = ""
-    @State var ntelefono = ""
+
     @State var seleccionado: Empleados?
     @State var empArray = [Empleados]()
 
 
     var body: some View {
-        NavigationView{
             VStack{
-                NavigationLink(destination: VStack{
-                    TextField("ID Empleado:", text: self.$nid).multilineTextAlignment(.center)
-                    TextField("Nombre Empleado:", text: self.$nempNombre).multilineTextAlignment(.center)
-                    TextField("Apellido Paterno", text: self.$nempApePat).multilineTextAlignment(.center)
-                    TextField("Apellido Materno", text: self.$nempApeMat).multilineTextAlignment(.center)
-                    TextField("Domicilio:", text: self.$ndomicilio).multilineTextAlignment(.center)
-                    TextField("Telefono:", text: self.$ntelefono).multilineTextAlignment(.center)
 
-                    Button("Guardar"){
-                        coreDM.guardarEmpleado(id: nid, empNombre: nempNombre, empApePat: nempApePat, empApeMat: nempApeMat, domicilio: ndomicilio, telefono: ntelefono)
-                        nid = ""
-                        nempNombre = ""
-                        nempApePat = ""
-                        nempApeMat = ""
-                        ndomicilio = ""
-                        ntelefono = ""
-                        mostrarEmpleado()
-                    }
-                    }){
-                    Text("Agregar")
-                }
+                    TextField("ID Empleado:", text: self.$id).textFieldStyle(RoundedBorderTextFieldStyle())
+                    TextField("Nombre Empleado:", text: self.$empNombre).textFieldStyle(RoundedBorderTextFieldStyle())
+                    TextField("Apellido Paterno", text: self.$empApePat).textFieldStyle(RoundedBorderTextFieldStyle())
+                    TextField("Apellido Materno", text: self.$empApeMat).textFieldStyle(RoundedBorderTextFieldStyle())
+                    TextField("Domicilio:", text: self.$domicilio).textFieldStyle(RoundedBorderTextFieldStyle())
+                    TextField("Telefono:", text: self.$telefono).textFieldStyle(RoundedBorderTextFieldStyle())
 
+                Button("Guardar"){
+                 if(seleccionado != nil){
+                    seleccionado?.id = id
+                    seleccionado?.empNombre = empNombre
+                    seleccionado?.empApePat = empApePat
+                    seleccionado?.empApeMat = empApeMat
+                    seleccionado?.domicilio = domicilio
+                    seleccionado?.telefono = telefono
+                    coreDM.actualizarEmpleado(empleado:seleccionado!)
+                    }else{
+                    coreDM.guardarEmpleado(id: id, empNombre: empNombre, empApePat: empApePat, empApeMat: empApeMat, domicilio: domicilio, telefono: telefono)
+                             }
+                            mostrarEmpleado()
+                            id = ""
+                            empNombre = ""
+                            empApePat = ""
+                            empApeMat = ""
+                            domicilio = ""
+                            telefono = ""
+                            seleccionado = nil
+                            }
                 List{
                     ForEach(empArray, id: \.self){
                         emp in
                         VStack{
+                            Text(emp.id ?? "")
                             Text(emp.empNombre ?? "")
+                            Text(emp.empApePat ?? "")
+                            Text(emp.empApeMat ?? "")
+                            Text(emp.domicilio ?? "")
+                            Text(emp.telefono ?? "")
                         }
                         .onTapGesture{
-                            seleccionado = emp
-                            id = emp.id ?? ""
-                        }
+                        seleccionado = emp
+                        id = emp.id ?? ""
+                        empNombre = emp.empNombre ?? ""
+                        empApePat =  emp.empApePat ?? ""
+                        empApeMat =  emp.empApeMat ?? ""
+                        domicilio =  emp.domicilio ?? ""
+                        telefono =  emp.telefono ?? ""
+                    }
+                        
                     }.onDelete(perform: {
                         indexSet in
                         indexSet.forEach({ index in
@@ -65,7 +83,7 @@ struct ContentView: View {
                 }.padding()
                     .onAppear(perform: {mostrarEmpleado()})
             }
-        }
+        
     }
     func mostrarEmpleado(){
             empArray = coreDM.leerTodosLosEmpleados()
@@ -77,4 +95,3 @@ struct ContentView_Previews: PreviewProvider {
         ContentView(coreDM: CoreDataManager())
     }
 }
-
